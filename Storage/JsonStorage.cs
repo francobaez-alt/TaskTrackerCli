4a8d7e8149e5b53ceb136b8b7d519ccc21a03154
unknown <franco.baez.dev@gmail.com>
2026-07-25 17:34:16 -0300

    JsonStorage service

---
+++ Storage/JsonStorage.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.Json;
+
+namespace TaskTrackerCli.Storage
+{
+    public class JsonStorage
+    {
+        private const string FilePath = "Tasks.json";
+
+        public JsonStorage()
+        {
+            if (!File.Exists(FilePath))
+                File.WriteAllText(FilePath, "[]");
+        }
+
+        public List<Task> GetTasks()
+        {
+            string json = File.ReadAllText(FilePath);
+
+            if(json == null)
+                return new List<Task>();
+
+            return JsonSerializer.Deserialize<List<Task>>(json);
+        }
+
+        public void SaveTasks(List<Task> tasks)
+        {
+            string json = JsonSerializer.Serialize(tasks, new JsonSerializerOptions
+            {
+                WriteIndented = true
+            });
+
+            File.WriteAllText(FilePath, json);
+              
+        }
+    }
+
+
+}
