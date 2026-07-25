14a2eb200eed66c2d8cc3cf822610a5a65c71b52
unknown <franco.baez.dev@gmail.com>
2026-07-25 17:12:39 -0300

    Initial commit

---
+++ Model/Task.cs
@@ -0,0 +1,13 @@
+﻿
+namespace TaskTrackerCli.Model
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string description { get; set; } = string.Empty;
+        public TaskStatus status { get; set; } = TaskStatus.ToDo;
+        public DateTime createdAt { get; set; } = DateTime.UtcNow;
+        public DateTime updatedAt { get; set; }
+    }
+}
+
---
+++ Model/TaskStatus.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTrackerCli.Model
+{
+    public enum TaskStatus
+    {
+        ToDo,
+        InProgress,
+        Done
+    }
+}
