fdc715a58838d1d9c30d09fcbd37b74519180b9e
unknown <franco.baez.dev@gmail.com>
2026-07-25 20:36:13 -0300

    ServiceResponse

---
+++ Model/ServiceResponse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskTrackerCli.Model
+{
+    public class ServiceResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+
+        public static ServiceResponse<T> Ok(T data, string message)
+        {
+            return new ServiceResponse<T>
+            {
+                Success = true,
+                Message = message,
+                Data = data
+            };
+        }
+
+        public static ServiceResponse<T> Error(string message)
+        {
+            return new ServiceResponse<T>
+            {
+                Success = false,
+                Message = message
+            };
+    }
+}
